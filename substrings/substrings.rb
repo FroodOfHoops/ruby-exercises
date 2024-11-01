efad5da95f33f598fcf69147d476ffd32c5a4363
James Norriss <nozzer15@hotmail.com>
2024-11-01 10:23:21 +0100

    created my substrings program

---
+++ substrings/substrings.rb
@@ -0,0 +1,19 @@
+word_list = ["a","test","sample","words","egg","cat","foo","ni"]
+
+def substrings(a_string, dictionary)
+  my_array = a_string.split(" ")
+  result = Hash.new(0)
+  my_array.each do |x|
+    dictionary.each do |y|
+      result[y] += 1 if x.include?(y)
+    end
+  end
+  result
+end
+
+# i'm sure i could simplify this if i could get my head around #reduce
+
+puts "Enter string:"
+my_string = gets.chomp.to_s
+
+puts substrings(my_string, word_list)
